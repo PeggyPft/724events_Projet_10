00a9b0448b85e54fcb24c12e4a66ff20999b13f8
peggyp <proffit.peggy@yahoo.fr>
2024-04-19 15:53:00 +0200

    add call function

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -16,6 +16,7 @@ const Form = ({ onSuccess, onError }) => {
       try {
         await mockContactApi();
         setSending(false);
+        onSuccess();
       } catch (err) {
         setSending(false);
         onError(err);
