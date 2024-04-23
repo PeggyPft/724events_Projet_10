3ac828a31b68ae5b897b9edd6013d886e4798431
peggyp <proffit.peggy@yahoo.fr>
2024-04-23 13:04:42 +0200

    filtre sur catégories

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -16,7 +16,7 @@ const EventList = () => {
   const filteredEvents = (
     (!type
       ? data?.events
-      : data?.events) || []
+      : data?.events.filter(event => event.type === type)) || []
   ).filter((event, index) => {
     if (
       (currentPage - 1) * PER_PAGE <= index &&
@@ -30,6 +30,7 @@ const EventList = () => {
     setCurrentPage(1);
     setType(evtType);
   };
+  
   const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
   const typeList = new Set(data?.events.map((event) => event.type));
   return (
