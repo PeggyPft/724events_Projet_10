4350f5b77aa25cba51a02a18340f3c48bca1c093
peggyp <proffit.peggy@yahoo.fr>
2024-04-24 18:28:24 +0200

    update

--- src/containers/Form/index.test.js
+++ src/containers/Form/index.test.js
@@ -21,8 +21,8 @@ describe("When Events is created", () => {
           bubbles: true,
         })
       );
-      await screen.findByText("Envoyer");
       await screen.findByText("En cours");
+      await screen.findByText("Envoyer");
       expect(onSuccess).toHaveBeenCalled();
     });
   });
