e13e68ff299a547338249876fc80bb4e07627094
peggyp <proffit.peggy@yahoo.fr>
2024-04-24 17:24:15 +0200

    reversing order of pictures from oldest to newest

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,7 +8,7 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
   const nextCard = () => {
     setTimeout(
